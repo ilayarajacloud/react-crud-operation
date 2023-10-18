9429e28c4cb56eb2e10f5777600d5a052cc7dd65
ilayarajacloud <ilayaraja@cloudlogic.tech>
2023-10-18 20:10:41 +0530

    crud

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Todo from './component/todo'
+import Crud from './component/crud'
 
 const App = () => {
   return (
     <div>
-      <Todo />
+      <Crud />
     </div>
   )
 }
