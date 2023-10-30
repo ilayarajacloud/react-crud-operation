16c0e15f39239ca2351942f163ed0e91e607bbc9
ilayarajacloud <ilayaraja@cloudlogic.tech>
2023-10-30 15:59:19 +0530

    scrach

--- src/component/crud.js
+++ src/component/crud.js
@@ -1,83 +1,45 @@
 import React, { useState } from 'react'
+import TableComponent from '../shared/tableComponent'
 import Form from '../shared/form'
-import TableComponent from '../shared/tableComponent';
-const studentInformation = [
-  { id: 1, fname: 'ilaya', lname: 'raja', email: 'ilaya@gmail.com', age: '29' },
-  { id: 2, fname: 'jancy', lname: 'rani', email: 'thanya@gmail.com', age: '29' },
-]
+
 const Crud = () => {
+  const initalState = [
+    { id: 1, name: 'ilaya', job: 'professional', location: 'bangalor' },
+    { id: 2, name: 'raja', job: 'professional', location: 'koimbatur' },
+    { id: 3, name: 'nana', job: 'deman', location: 'kerala' },
+    { id: 4, name: 'nun', job: 'gost', location: 'hydrabad' },
+  ]
+  const [userList, setUserList] = useState(initalState);
+  const [update, setUpdate] = useState("")
 
-
-  const [studentList, setStudentList] = useState(studentInformation);
-  const [updateStudent, setUpdateStudent] = useState({})
-  const [filterData, setFilterData] = useState('');
-  const [filterGmail, setFilterGmail] = useState('');
-  const [filterLastName, setFilterLastName] = useState("");
-  const [filterAge, setFilterAge] = useState('');
-
-  console.log(studentList);
-
-  const findSearch = (e) => {
-    const { value } = e.target
-    setFilterData(value);
-  } 
-
-  const findGmail=(e)=>{
-    setFilterGmail(e.target.value)
+  const removeUserList = (id) => {
+    const result = userList.findIndex((item) => item.id === id)
+    const res = userList[result]
+    setUpdate(res);
   }
 
-  const findLastName=(e)=>{
-    setFilterLastName(e.target.value)
-  }
-  const findAge=(e)=>{
-    setFilterAge(e.target.value)
-  }
-  console.log(filterAge);
-
-  const resultData = studentList.filter((item) =>
-    item.fname.toLowerCase().includes(filterData.toLowerCase())
-     && item.email.toLowerCase().includes(filterGmail.toLowerCase())
-     && item.lname.toLowerCase().includes(filterLastName.toLowerCase())
-     && item.age.toLocaleUpperCase().includes(filterAge.toLowerCase())
-  )
-
-  // const resultData=studentList.filter((item)=>{
-  //   const search = filterData.toLowerCase();
-  //   return Object.values(item).some((value)=> value.toLowerCase().includes(search));
-  // })
-
-  const addStudentList = (val) => {
-    if (updateStudent.id) {
-      // debugger;
-      const findIndex = studentList.findIndex((item) => item.id === updateStudent.id)
-      console.log(findIndex);
-      studentList[findIndex] = val;
-      setStudentList([...studentList])
+  const addUserList = (val) => {
+    debugger;
+    console.log(val);
+    if (update.id) {
+     const find=userList.findIndex((item)=>item.id===update.id)
+     userList[find] =val;
+     setUserList([...userList])
     } else {
-      val.id = studentList.length + 1;
-      setStudentList([...studentList, val])
-      console.log(val);
+      val.id = userList.length + 1;
+      setUserList([...userList, val])
     }
-
   }
 
-  const editStudent = (id) => {
-    const res = studentList.findIndex((item) => item.id === id)
-    const result = studentList[res]
-    setUpdateStudent(result);
+  const deleteUserList = (id) => {
+    const result = userList.filter((item) => item.id !== id)
+    setUserList(result);
   }
-  console.log(updateStudent);
 
-
-  const deleteStudent = (id) => {
-    const find = studentList.filter((item) => item.id !== id)
-    setStudentList(find);
-    console.log(find);
-  }
   return (
     <>
-      <Form addStudentList={addStudentList} updateStudent={updateStudent} />
-      <TableComponent data={resultData} deleteStudent={deleteStudent} editStudent={editStudent} findSearch={findSearch} findGmail={findGmail} findLastName={findLastName} findAge={findAge}/>
+      <TableComponent data={userList} deleteUserList={deleteUserList} removeUserList={removeUserList} />
+      <Form addUserList={addUserList} update={update} />
     </>
   )
 }
--- src/shared/form.js
+++ src/shared/form.js
@@ -1,65 +1,54 @@
 import React, { useEffect, useState } from 'react'
 
-const Form = ({ addStudentList, updateStudent }) => {
-  const initialState = { id: null, fname: '', lname: '', email: '', age: '' }
-  const [userInput, setUserInput] = useState(initialState);
 
-  const handleChange = (e) => {
+const Form = ({ addUserList, update }) => {
+  const initalFormState = { id: null, name: '', job: '', location: '' };
+  const [form, setForm] = useState(initalFormState);
+
+  const onChangeInput = (e) => {
     const { name, value } = e.target
-    setUserInput({ ...userInput, [name]: value })
+    setForm(
+      { ...form, [name]: value }
+    )
   }
 
   useEffect(() => {
-    setUserInput(updateStudent)
-  }, [updateStudent])
+    setForm(update);
+  }, [update])
+  console.log(form);
 
-  const addFormData = (e) => {
+  const addForm = (e) => {
     e.preventDefault();
-    if (!userInput.fname || !userInput.email) return
-    addStudentList(userInput);
-    setUserInput(initialState)
+    addUserList(form)
+    setForm(initalFormState)
   }
-  console.log(userInput);
 
-  
-
+  console.log(form);
   return (
-    <>
-      <form>
-        <label>First Name</label>
-        <input
-          type='text'
-          name='fname'
-          value={userInput.fname}
-          onChange={handleChange}
-        />
-        <br></br>
-        <label>Last name</label>
-        <input
-          type='text'
-          name='lname'
-          value={userInput.lname}
-          onChange={handleChange}
-        />
-        <br></br>
-        <label>Email</label>
-        <input
-          type='email'
-          name='email'
-          value={userInput.email}
-          onChange={handleChange}
-        />
-        <br></br>
-        <label>Age</label>
-        <input
-          type='number'
-          name='age'
-          value={userInput.age}
-          onChange={handleChange}
-        />
-        <button onClick={addFormData}>Submit</button>
-     </form>
-    </>
+    <form>
+      <label>Name</label>
+      <input
+        type='text'
+        name='name'
+        value={form.name}
+        onChange={onChangeInput}
+      />
+      <label>Job</label>
+      <input
+        type='text'
+        value={form.job}
+        name='job'
+        onChange={onChangeInput}
+      />
+      <label>Location</label>
+      <input
+        type='text'
+        name='location'
+        value={form.location}
+        onChange={onChangeInput}
+      />
+      <button onClick={addForm}>Submit</button>
+    </form>
   )
 }
 
