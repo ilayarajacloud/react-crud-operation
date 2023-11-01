35fee53e233a179585ff13703770b63b2de3ea13
ilayarajacloud <ilayaraja@cloudlogic.tech>
2023-11-01 17:27:05 +0530

    slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import Tabs from './component/Tabs'
+import React, { useState } from 'react'
+import data from './data';
 
 const App = () => {
-  
+  const [people, setPepole] = useState(data);
+  const [index, setIndex] = useState(0);
+  console.log(people);
   return (
-    <div>
-      <Tabs />
-    </div>
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className='section-center'>
+        {people.map((item, index) => {
+          const { id, name, image, quote, title } = item;
+          return (
+            <article key={id}>
+              <img src={image} />
+            </article>
+          )
+
+        })}
+      </div>
+    </section>
   )
 }
 
